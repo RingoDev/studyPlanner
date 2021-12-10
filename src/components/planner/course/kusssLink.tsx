f12880ddda679ce41bbf1d914385b6775055546d
RingoDev <ringodev@protonmail.com>
2021-12-10 19:30:07 +0100

    refactored styling of all components

--- src/components/planner/course/kusssLink.tsx
+++ src/components/planner/course/kusssLink.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const KusssLink: React.FC<{ id: string }> = ({id, children}) => (
     <Link target={"_blank"}
           href={"https://kusss.jku.at/kusss/coursecatalogue-get-courseclasses.action?grpCode=" + id}
-          color={"text.primary"}>
+          color={"text.primary"} sx={{textDecoration: "none"}}>
         {children}
     </Link>
 )
