faa7e1f3022913414d630c9965c63f78f7ba241e
RingoDev <ringodev@protonmail.com>
2022-01-22 10:38:14 +0100

    kusss link restyling to make UX better

--- src/components/progress/courses/kusss-link.tsx
+++ src/components/progress/courses/kusss-link.tsx
@@ -1,15 +1,22 @@
 import { Link } from "@mui/material";
 import React from "react";
+import { styled } from "@mui/material/styles";
+
+const StyledLink = styled(Link)(() => ({
+  textDecoration: "none",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+}));
 
 const KusssLink: React.FC<{ id: string }> = ({ id, children }) => (
-  <Link
+  <StyledLink
     target={"_blank"}
     href={`https://kusss.jku.at/kusss/coursecatalogue-get-courseclasses.action?grpCode=${id}`}
     color={"text.primary"}
-    sx={{ textDecoration: "none" }}
   >
     {children}
-  </Link>
+  </StyledLink>
 );
 
 export default KusssLink;
